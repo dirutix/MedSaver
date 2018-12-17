c1a4242b5fbe8f8869a68bd09a50878ea822c1e1
DimaAndry <dirutix@list.ru>
2018-12-18 01:11:58 +0200

    analysisCalc done

--- src/analisysCalculatorPage.js
+++ src/analisysCalculatorPage.js
@@ -1,17 +1,14 @@
-const {Composite, Page, TextView, Picker, Slider, Button, ScrollView} = require('tabris');
+/* eslint-disable linebreak-style */
+/* eslint-disable max-len */
+/* eslint-disable linebreak-style */
+/* eslint-disable semi */
+/* eslint-disable no-trailing-spaces */
+/* eslint-disable indent */
+/* eslint-disable quotes */
+/* eslint-disable no-unused-vars */
+const {Composite, Page, TextView, Picker, Slider, Button, TextInput, ScrollView} = require('tabris');
 
 const MARGIN = 15;
-const TEXT = 'There was nothing so very remarkable in that; nor did Alice ' +
-  'think it so very much out of the way to hear the Rabbit say to itself, ‘Oh ' +
-  'dear! Oh dear! I shall be late!’ (when she thought it over afterwards, it ' +
-  'occurred to her that she ought to have wondered at this, but at the time ' +
-  'it all seemed quite natural); but when the Rabbit actually took a watch ' +
-  'out of its waistcoat-pocket, and looked at it, and then hurried on, Alice ' +
-  'started to her feet, for it flashed across her mind that she had never ' +
-  'before seen a rabbit with either a waistcoat-pocket, or a watch to take ' +
-  'out of it, and burning with curiosity, she ran across the field after it, ' +
-  'and fortunately was just in time to see it pop down a large rabbit-hole ' +
-  'under the hedge.';
 
 let trayState = 'down';
 let trayHeight;
@@ -19,175 +16,108 @@ let dragOffset;
 
 const page = new Page({
   id: 'title',
-  title: 'Калькулятор штамів',
+  title: 'Калькулятор аналізів',
   autoDispose: false
 });
 
-const medcineList = [
+const analysisList = [
   {
-    name: "Анальгін",
-    child: true,
-    adult: true,
-    kilogramsDozing: false,
-    childKgLimit: null,
-    mlPerKgList: [],
-    childAgeRangeEl: [12, 14, 18],
-    childDozes: ["½ таблетки", "½-1 таблетка"],
-    adultDoze: "½-1 таблетка"
+    name: "Кліренс Креатиніну",
+    params: ["Вага(кг):", "Конц. креатиніну у сечі(ммоль/л):", "Конц. креатиніну у сиворотці(мкмоль/л):", "Хвилинний діурез(мл/1440хв):"],
+    result: (weight, piss, serum, min) => {
+      let s = (4 * weight + 7)/(weight + 90);
+      let c = (piss * 1000 * min)/serum;
+      return c * 1.73 / s;
+    },
+    measure: "од."
   },
   {
-    name: "Анаприлін",
-    child: true,
-    adult: true,
-    kilogramsDozing: true,
-    childKgLimit: null,
-    childAgeRangeEl: [3, 18],
-    mlPerKgList: [0,25],
-    childDozes: [],
-    adultDoze: "40 мг"
+    name: "Кальцій іонізований",
+    params: ["Кальцій:", "Aльбумін крові(од)):"],
+    result: (ca, alb) => {
+      return ca * 878/(alb * 15.05 + 1053);
+    },
+    measure: "моль/літр"
   },
   {
-    name: "Аспаркам",
-    child: false,
-    adult: true,
-    kilogramsDozing: false,
-    childKgLimit: null,
-    childAgeRangeEl: [],
-    mlPerKgList: [],
-    childDozes: [],
-    adultDoze: "1 таблетка"
+    name: "Лейкоцитарний індекс інтоксикації",
+    params: ["Міелоцити:", "Метаміелоцити:", "Палочкоядерні:", "Сегментоядерні:", "Площа клітини:", "Лімфоцити:", "Моноцити:", "Еозинофіли:"],
+    result: (mi, met, branch, seg, square, limf, mon, eos) => {
+      return (4 * mi + 3 * met + 2 * branch + seg) * (square + 1)/((limf + mon)*(eos + 1));
+    },
+    measure: "од."
   },
   {
-    name: "Аугментин",
-    child: true,
-    adult: true,
-    kilogramsDozing: true,
-    childKgLimit: 40,
-    childAgeRangeEl: [0, 18],
-    mlPerKgList: [25],
-    childDozes: [],
-    adultDoze: "1 таблетка"
+    name: "Середній об'єм еритроцита",
+    params: ["Гематокрит(%):", "Кількість еритроцитів(10^12/л):"],
+    result: (hem, quant) => {
+      return hem * 10/quant;
+    },
+    measure: "фл(фемолітр)"
   },
   {
-    name: "Бісопролол",
-    child: false,
-    adult: true,
-    kilogramsDozing: false,
-    childKgLimit: null,
-    childAgeRangeEl: [],
-    mlPerKgList: [],
-    childDozes: [],
-    adultDoze: "1,25 мг"
-  },
-  {
-    name: "Ванкоміцин",
-    child: false,
-    adult: true,
-    kilogramsDozing: false,
-    childKgLimit: null,
-    childAgeRangeEl: [0, 12, 18],
-    mlPerKgList: [],
-    childDozes: [10, 500],
-    adultDoze: "500 мг"
+    name: "Концентрація гемоглобіну в еритроциті",
+    params: ["Гемоглобін(г/дл):", "Гематокрит(%):"],
+    result: (hb, ht) => {
+      return hb * 100/ht;
+    },
+    measure: "г/дл"
   }
 
 ]
 
-const MEDCINE = ['Аспаркам',
-'Біфрен',
-'Гропринозин',
-'Ереспал',
-'Ібупрофен',
-'Ікервис',
-'Клотримазол',
-'Магне-В6',
-'Мукалтин',
-'Німесил',
-'Новірин',
-'Но-шпа',
-'Олембік-Н',
-'Омез',
-'Парацетамол',
-'Синупрет',
-'Тіотриазолін',
-'Фарлінекс',
-'Флуконазол',
-'Целіста'];
-
 new TextView({
-  id: 'medcineLabel',
+  id: 'analysisLabel',
   left: MARGIN, 
   top:   '#title 18', 
   width: 120,
-  text: 'Препарат:'
+  font: '18px',
+  text: 'Аналіз:'
 }).appendTo(page);
 
 new Picker({
-  id: 'medcinePicker',
-  left: '#medcineLabel 15', 
+  id: 'analysisPicker',
+  left: '#analysisLabel 15', 
   right: MARGIN, 
-  baseline: '#medcineLabel',
-  itemCount: medcineList.length,
-  itemText: index => medcineList[index].name
-}).appendTo(page);
+  baseline: '#analysisLabel',
+  itemCount: analysisList.length,
+  itemText: index => analysisList[index].name
+}).on('select', ({index}) => {
+  let block = page.find("#parameters");
+  block.children().dispose();
+  analysisList[index].params.forEach((value, index, arr) => {
+    new TextView({
+      id: 'argument' + index,
+      top: '#argument' + (index-1) + ' 10',
+      width: 150,
+      font: '18px',
+      text: value
+    }).appendTo(block);
 
-new TextView({
-  id: 'ageLabel',
-  text: 'Вік:',
-  left: MARGIN,
-  width: 120,
-  top: '#medcineLabel 18'
-}).appendTo(page);
-
-const AGE = [];
-for(let i = 0; i < 100; i++)
-  AGE[i] = String(i);
-
-new Picker({
-  id: 'agePicker',
-  right: MARGIN,
-  baseline: '#ageLabel',
-  left: '#ageLabel 15',
-  itemCount: AGE.length,
-  itemText: index => AGE[index]
-}).appendTo(page);
+    new TextInput({
+      alignment: 'left',
+      left: '#argument' + index + ' 10',
+      top: '#argument' + (index-1),
+      width: 80,
+      borderColor: 'yellow',
+      message: 'Type here',
+      keyboard: 'number'
+    }).appendTo(block);
+  })
+})
+.appendTo(page);
 
 new Composite({
-  id: 'weightPanel',
-  left: MARGIN, 
-  top: '#ageLabel 18', 
+  id: 'parameters',
+  left: MARGIN,
+  top: '#analysisPicker 10',
   right: MARGIN
-}).append(
-  new TextView({
-    id: 'weightLabel',
-    left: 0, 
-    centerY: 0, 
-    width: 120,
-    text: 'Маса тіла:'
-  })
-).append(
-  new TextView({
-    id: 'weight',
-    right: MARGIN, 
-    centerY: 0, 
-    width: 50,
-    text: '0 Кг'
-  })
-).append(
-  new Slider({
-    id: 'weightSlider',
-    left: '#weightLabel 15', 
-    right: '#weight 15', 
-    centerY: 0
-  }).on('selectionChanged', ({value}) => {
-    page.find('#weight').set('text', value + ' Кг');
-  })
-).appendTo(page);
+}).appendTo(page);
 
 new Button({
   id: 'calculateButton',
   text: 'Порахувати',
-  top: '#weightPanel 18',
+  top: '#parameters 18',
   height: 50,
   width: 140,
   centerX: 0
@@ -195,15 +125,38 @@ new Button({
   calculate();
 }).appendTo(page);
 
+new TextView({
+  id: "horizontalStripe",
+  left: 0, 
+  right: 0, 
+  top: '#calculateButton 30',
+  font: "2px",
+  background: "#009AFD",
+  width: 2
+}).appendTo(page);
+
+let message = new TextView({
+  id: "message",
+  left: MARGIN, 
+  right: MARGIN, 
+  top: '#horizontalStripe 30',
+  font: "bold 24px",
+  textColor: "#009AFD"
+}).appendTo(page);
+
 function calculate() {
-  /* message.text = [
-    'Flight booked for: ' + page.children('#nameInput').first().text,
-    'Destination: ' + AGE[page.children('#countryPicker').first().selectionIndex],
-    'Seating: ' + createSeating(),
-    'Luggage: ' + createWeight(),
-    'Meal: ' + createMeal(),
-    'Redeem miles: ' + createFrequentFlyerInfo()
-  ].join('\n') + '\n';*/
+  let params = page.find(TextInput);
+  let map = Array.prototype.map;
+  let index = page.find("#analysisPicker").get('selectionIndex');
+  let analys = analysisList[index];
+  params = map.call(params, function(item) {return item.text});
+  params = params.filter(function(n) {return n !== ''});
+  if(params.length < analys.params.length){
+    message.text = "Заповніть усі поля, будь-ласка";
+  } else {
+    message.text = analys.name + " = " + analys.result(...params) + " " + analys.measure;
+  }
+
 }
 /*
 function createSeating() {
